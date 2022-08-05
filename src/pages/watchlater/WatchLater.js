31ebd297187b14896e532ad577a3bd16745a49ac
Mohitk391 <mohitkpatel391@gmail.com>
2022-08-05 23:17:11 +0530

    doc: improved text structure

--- src/pages/watchlater/WatchLater.js
+++ src/pages/watchlater/WatchLater.js
@@ -22,7 +22,7 @@ const WatchLater = () => {
                     ) 
                 })
                 ) : (
-                    <div className="flex flex-center">
+                    <div className="flex flex-column flex-center">
                         <div>You don't have any video in Watch Later section</div>
                         <div>Check out <Link to="/explore">Explore </Link> to add videos in Watch Later</div>
                     </div>
